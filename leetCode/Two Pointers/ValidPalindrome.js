70b7cb0db8358f317c4ba1f8ac8e6b35e8120212
Akhil <hakhilkumar@gmail.com>
2025-07-10 22:52:01 -0500

    Day 3

---
+++ leetCode/Two Pointers/ValidPalindrome.js
@@ -0,0 +1,11 @@
+
+// https://leetcode.com/problems/valid-palindrome/description/
+
+const isPalindrome = (s) => {
+
+
+}
+
+const s = "A man, a plan, a canal: Panama"
+
+console.log(isPalindrome(s))
